08e51cdb3dde5067c5bb38d205dda63db82d1259
zokatr <ahmetgazi03@gmail.com>
2024-01-07 22:17:46 +0300

    Init

---
+++ flightpy/flightrl/setup.py
@@ -0,0 +1,30 @@
+
+import os
+import re
+import sys
+import platform
+import subprocess
+
+from setuptools import setup, Extension, find_packages
+from setuptools.command.build_ext import build_ext
+from distutils.version import LooseVersion
+
+setup(
+    name='flightrl',
+    version='0.0.1',
+    author='Yunlong Song',
+    author_email='song@ifi.uzh.ch',
+    description='Flightmare: A Quadrotor Simulator.',
+    long_description='',
+    packages=find_packages(),
+)
+
+# setup(
+#     name='rpg_baselines',
+#     version='0.0.1',
+#     author='Yunlong Song',
+#     author_email='song@ifi.uzh.ch',
+#     description='Flightmare: A Quadrotor Simulator.',
+#     long_description='',
+#     packages=['rpg_baselines'],
+# )
